5b979100acf934c5058a4b68b57d79957fa287a5
mrfhmd7 <mrf.hmd7@gmail.com>
2023-04-25 16:49:44 +0600

    post added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <LoadPosts></LoadPosts>
       <District name='NKhali' special='bivag'></District>
       <District name='Bbaria' special='jodha Akbar'></District>
       <District name='Sumilla' special='moyna and moti'></District>
+    </div>
+  );
+};
+
+function LoadPosts() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(res => res.json())
+    .then(data => setPosts(data));
+  } ,[])
+  return (
+    <div>
+      <h1>Post: {posts.length}</h1>
+      {
+        posts.map(post => <Post
+          title={post.title}
+          body={post.body}
+        ></Post>)
+      }
+    </div>
+  );
+};
+
+function Post(props) {
+  return (
+    <div style={{backgroundColor:'tomato', margin:'10px', padding:'10px', borderRadius:'8px', border:'3px solid red' }}>
+      <h3>Title: {props.title}</h3>
+      <p style={{color:'white'}}>Body: {props.body}</p>
     </div>
   );
 };
