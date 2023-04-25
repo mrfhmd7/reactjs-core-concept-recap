120a706264b37ce3adf42eff610fac635f59d0bb
mrfhmd7 <mrf.hmd7@gmail.com>
2023-04-25 14:29:50 +0600

    District added

--- src/App.js
+++ src/App.js
@@ -4,9 +4,27 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-
+      <District name='NKhali' special='bivag'></District>
+      <District name='Bbaria' special='jodha Akbar'></District>
+      <District name='Sumilla' special='moyna and moti'></District>
     </div>
   );
+};
+
+const districtStyle = {
+  backgroundColor: 'tomato',
+  margin: '15px',
+  borderRadius: '10px',
+  padding: '10px'
 }
+
+function District(props) {
+  return (
+    <div style={districtStyle}>
+      <h1>Name: {props.name}</h1>
+      <p>Specialty: {props.special}</p>
+    </div>
+  );
+};
 
 export default App;
